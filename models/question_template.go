8b8a3d8ef96c3f0abf822d1bb9092e12c75f5f88
kei.sugano@veltra.com <kei@veltra.com>
2023-11-20 16:23:06 +0900

    lang_id added

--- models/question_template.go
+++ models/question_template.go
@@ -7,6 +7,7 @@ import (
 type QuestionTemplate struct {
 	ID        uint      `gorm:"type:int unsigned;primary_key;auto_increment" json:"id"`
 	Name      string    `gorm:"type:varchar(255)" json:"name"`
+	LangID    int       `gorm:"type:int unsigned;index"`
 	CreatedAt time.Time `gorm:"type:datetime" json:"created_at"`
 	UpdatedAt time.Time `gorm:"type:datetime" json:"updated_at"`
 
