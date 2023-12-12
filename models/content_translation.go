0fd81a790f4c5112052e180153da70728cdf0245
kei.sugano@veltra.com <kei@veltra.com>
2023-12-12 09:16:26 +0900

    table comment on content translation added.

--- models/content_translation.go
+++ models/content_translation.go
@@ -15,10 +15,10 @@ const (
 
 type ContentTranslation struct {
 	ID                  uint    `gorm:"type:int unsigned;primaryKey;autoIncrement"`
-	TranslatedContent   string  `gorm:"type:varchar(1000)"`
-	ContentType         string  `gorm:"type:enum('reply', 'review', 'image')" json:"type"`
-	ContentID           uint64  `gorm:"type:int unsigned;index;foreignKey:Content" json:"content_id"`
-	LangID              int     `gorm:"type:int unsigned;index"`
-	Translator          string  `gorm:"type:enum('google', 'deepl')" json:"type"`
-	HumanApprovalID     uint    `gorm:"type:int unsigned" json:"content_id"`
+        TranslatedContent   string  `gorm:"type:varchar(1000)" comment: "translated content here."`
+        ContentType         string  `gorm:"type:enum('reply', 'review', 'image')" json:"type" comment: "translation of which content (reply/review/image)"`
+        ContentID           uint64  `gorm:"type:int unsigned;index;foreignKey:Content" json:"content_id" comment: "since this is child table of content of [content_type], this column clarify which this instance belongs to."`
+        LangID              int     `gorm:"type:int unsigned;index" comment: "The language ID of the translation object"`
+        Translator          string  `gorm:"type:enum('google', 'deepl')" json:"type" comment: "Google, DeepL, etc... Translation Service identifier"`
+        HumanApprovalID     uint    `gorm:"type:int unsigned" json:"human_approval_id" comment: "when translation approved, the ID of the user who approved the translation."`
 }
